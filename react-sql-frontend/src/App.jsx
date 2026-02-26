73926171e029693ba300f166e11c4329854ed18d
Keith Charlton <charltonkeith8@gmail.com>
2026-02-26 16:58:31 -0500

    added version badge

--- react-sql-frontend/src/App.jsx
+++ react-sql-frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Users from "./Users";
 import "./App.css";
+import packageJson from "../package.json";
+
+const APP_VERSION = packageJson?.version || "0.0.0";
 
 function App() {
   return (
     <div className="App">
       <Users />
+      <div className="app-version-badge" aria-label={`App version ${APP_VERSION}`}>
+        v{APP_VERSION}
+      </div>
     </div>
   );
 }
